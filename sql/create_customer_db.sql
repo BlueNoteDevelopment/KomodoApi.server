95d0ee382b54be7676ad1d41b966b6b06babbe1e
swm03 <swm03@LAPTOP-LS1CVVBG>
2017-07-02 13:39:18 -0400

    Add sql for user add

--- sql/create_customer_db.sql
+++ sql/create_customer_db.sql
@@ -8,3 +8,10 @@ CREATE TABLE `migration` (
 DELETE FROM mysql.user WHERE Host='localhost' AND User='customer1_root';
 GRANT ALL PRIVILEGES ON customer1_komodo_db.* TO 'customer1_root'@'localhost' IDENTIFIED BY 'customer1_1@2#API';
 FLUSH PRIVILEGES;
+
+
+--First User
+/*
+INSERT INTO user_account (user_account_name,encrypted_password,is_active,user_token_guid,created_datetime)
+	VALUES('test','$2y$10$71eaOBA9wHKZjmVIhAmij.uTgc2kIhNb8MnyhzzXlXTaDWJc5ZE/q',1,UUID(),now())
+*/
